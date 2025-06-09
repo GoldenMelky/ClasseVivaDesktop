60d30b543459fa1e99d06da0f4a24365a1d8dbee
Golden Melky <goldenextra8@outlook.it>
2025-06-09 23:49:24 +0200

    major update

--- cvvApi.py
+++ cvvApi.py
@@ -17,61 +17,68 @@ class Utente:
         self.username = username
         self.password = password
         endpoint = apiUrl + "v1/auth/login"
-        body=f'{{"ident": null, "pass": "{self.password}", "uid": "{self.username}"}}'
-        self.login = json.loads(requests.post(endpoint, data=body, headers=headers).text)
-        self.login.update({"studendId": self.username[1:-1]})
+        body=json.dumps({
+            "ident": None,
+            "pass": self.password,
+            "uid": self.username
+        })
+        req = requests.post(endpoint, data=body, headers=headers)
+        if "WrongCredentials" in req.text:
+            raise ValueError("Wrong username or password.")
+        self.login = json.loads(req.text)
+        self.login.update({"studentId": self.username[1:-1]})
         headers.update({"Z-Auth-Token": self.login["token"]})
-        self.headers = headers
+        self.headers = headers.copy()
         print(f'Logged as: {" ".join([self.login[key] for key in ["firstName", "lastName"]])}')
     
     def agenda(self,begin: str, end: str = ""):
         if end == "":
             end = begin
-        endpoint = apiUrl + f"v1/students/{self.login["studendId"]}/agenda/all/{begin}/{end}"
+        endpoint = apiUrl + f"v1/students/{self.login["studentId"]}/agenda/all/{begin}/{end}"
         response = requests.get(endpoint, headers=self.headers)
         return json.loads(response.text)
     def assenze(self):
-        endpoint = apiUrl + f"v1/students/{self.login['studendId']}/absences/details"
+        endpoint = apiUrl + f"v1/students/{self.login['studentId']}/absences/details"
         response = requests.get(endpoint, headers=self.headers)
         return json.loads(response.text)
     def didattica(self, contentId: str = ""):
-        endpoint = apiUrl + f"v1/students/{self.login['studendId']}/didactics"
+        endpoint = apiUrl + f"v1/students/{self.login['studentId']}/didactics"
         if contentId:
             endpoint = endpoint + "/item/" + contentId
         response = requests.get(endpoint, headers=self.headers)
         return json.loads(response.text)
     def bacheca(self):
-        endpoint = apiUrl + f"v1/students/{self.login['studendId']}/noticeboard"
+        endpoint = apiUrl + f"v1/students/{self.login['studentId']}/noticeboard"
         response = requests.get(endpoint, headers=self.headers)
         return json.loads(response.text)
     def libri(self):
-        endpoint = apiUrl + f"v1/students/{self.login['studendId']}/schoolbooks"
+        endpoint = apiUrl + f"v1/students/{self.login['studentId']}/schoolbooks"
         response = requests.get(endpoint, headers=self.headers)
         return json.loads(response.text)
     def voti(self):
-        endpoint = apiUrl + f"v1/students/{self.login['studendId']}/grades"
+        endpoint = apiUrl + f"v1/students/{self.login['studentId']}/grades"
         response = requests.get(endpoint, headers=self.headers)
         print(response.text)
         return json.loads(response.text)
     def note(self):
-        endpoint = apiUrl + f"v1/students/{self.login['studendId']}/notes/all"
+        endpoint = apiUrl + f"v1/students/{self.login['studentId']}/notes/all"
         response = requests.get(endpoint, headers=self.headers)
         return json.loads(response.text)
     def periodi(self):
-        endpoint = apiUrl + f"v1/students/{self.login['studendId']}/periods"
+        endpoint = apiUrl + f"v1/students/{self.login['studentId']}/periods"
         response = requests.get(endpoint, headers=self.headers)
         return json.loads(response.text)
     def materie(self):
-        endpoint = apiUrl + f"v1/students/{self.login['studendId']}/subjects"
+        endpoint = apiUrl + f"v1/students/{self.login['studentId']}/subjects"
         response = requests.get(endpoint, headers=self.headers)
         return json.loads(response.text)
     def documenti(self):
-        endpoint = apiUrl + f"v1/students/{self.login['studendId']}/documents"
+        endpoint = apiUrl + f"v1/students/{self.login['studentId']}/documents"
         response = requests.get(endpoint, headers=self.headers)
         return json.loads(response.text)
     def lezioni(self, begin: str, end: str = ""):
         if end == "":
             end = begin
-        endpoint = apiUrl + f"v1/students/{self.login['studendId']}/lessons/{begin}/{end}"
+        endpoint = apiUrl + f"v1/students/{self.login['studentId']}/lessons/{begin}/{end}"
         response = requests.get(endpoint, headers=self.headers)
         return json.loads(response.text)
--- main.py
+++ main.py
@@ -1,15 +1,84 @@
 import cvvApi
-import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+from tkcalendar import *
 import json
+from datetime import datetime
+from threading import Thread
+from time import sleep
+from PIL import Image, ImageTk
 
-with open("credenziali.json", "r") as file:
-    creds = json.loads(file.read())
-    username = creds["username"]
-    password = creds["password"]
+splash = Tk()
+frame = Frame(splash, background="#e40b29")
+frame.pack(fill=BOTH, expand=True)
+splash.title("Login...")
+splash.geometry("317x150")
+spalshImage = ImageTk.PhotoImage(Image.open("classevivaSplash.png").resize((317, 100)))
+panel = Label(frame, image=spalshImage, background="#e40b29")
+panel.pack()
+loginLabel = Label(frame, text="Login in corso...", background="#e40b29", foreground="white", font=(16))
+loginLabel.pack()
 
-user = cvvApi.Utente(username, password)
+
+def loginButton():
+    password = password_entry.get()
+    username = username_entry.get()
+    try:
+        global user 
+        user = loginFunc(username, password)
+        login.destroy()
+    except Exception as e:
+        error_label.config(text=f"Errore: {str(e)}")
+        error_label.pack(pady=5)
+        return
+
+def loginFunc(username,password):
+    global user
+    user = cvvApi.Utente(username, password)
+    loginLabel.config(text=f"Ciao {user.login["firstName"].title()}")
+    with open("credenziali.json", "w") as file:
+        creds = {"username":username,"password":password}
+        file.write(str(json.dumps(creds)))
+    splash.destroy()
+    
+
+try:
+    with open("credenziali.json", "r") as file:
+        creds = json.loads(file.read())
+        username = creds["username"]
+        password = creds["password"]
+        t = Thread(target=loginFunc, args=(username, password))
+        t.start()
+except FileNotFoundError:
+    login = Tk()
+    login.attributes(topmost=True)
+    login.title("Login")
+    login.geometry("350x200")
+    login.resizable(False, False)
+    
+    username = StringVar()
+    password = StringVar()
+
+    username_label = ttk.Label(login, text="Username:")
+    username_label.pack(pady=5)
+    username_entry = ttk.Entry(login,textvariable=username)
+    username_entry.pack(pady=5)
+    username_entry.focus()
+
+    password_label = ttk.Label(login, text="Password:")
+    password_label.pack(pady=5)
+    password_entry = ttk.Entry(login,textvariable=password, show="*")
+    password_entry.pack(pady=5)
+
+    error_label = ttk.Label(login, text="", foreground="red")
+    
+    login_button = ttk.Button(login, text="Login", command=loginButton)
+    login_button.pack(pady=5)
+    login.mainloop()
 
 def today(data: str=""):
+    if not data:
+        data = datetime.now().strftime("%Y%m%d")
     output = []
     lezioni = user.lezioni(data)
     for lezione in lezioni["lessons"]:
@@ -19,7 +88,7 @@ def today(data: str=""):
         else:
             title = lezione['authorName']
             subtitle = ""
-        subtitle += f"{lezione["evtHPos"]}° ora"
+        subtitle += f"{lezione['evtHPos']}° ora"
         output.append({
             "title": title,
             "subtitle": subtitle,
@@ -76,12 +145,14 @@ def voti():
         })
     return output
 
-window = tk.Tk()
-window.title("CVV Desktop")
-window.geometry("600x400")
-first_button = tk.Button(text="Saluta!", command=voti)
-first_button.grid(row=0, column=0, sticky="WE")
-irst_button = tk.Button(text="Saluta!", command=voti)
-irst_button.grid(row=1, column=1)
-window.columnconfigure(0, weight=3)
-window.mainloop()
+def main():
+    root = Tk()
+    root.title("ClasseViva")
+    root.geometry("300x200")
+
+    # cal = Calendar(root,date_pattern="YYYYMMDD")
+    # cal.pack()
+    # cal.bind("<<CalendarSelected>>", lambda e: print(today(cal.get_date())))
+    root.mainloop()
+splash.mainloop()
+#main()
